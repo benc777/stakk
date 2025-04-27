b1a7d54c63b78516c18ede3d96cc6bb79fc72843
benc777 <80216843+benc777@users.noreply.github.com>
2025-04-27 09:52:37 -0400

    Initial commit from stakk-website

---
+++ scripts/dark_mode.js
@@ -0,0 +1,11 @@
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', document.body.classList.contains('dark-mode') ? 'true' : 'false');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('darkMode') === 'true') {
+    document.body.classList.add('dark-mode');
+  }
+});
